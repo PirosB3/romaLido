59f8698caee1113225af966a1e13ac58c3786a70
Daniel Pyrathon <pirosb3@gmail.com>
2015-01-24 14:02:17 +0100

    added timetable

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,6 +1,6 @@
-var DIRECTIONS = [
-    'towards-rome',
-    'towards-ostia'
+var DIRECTIONS_TO_SCOPE = [
+    ['towards-rome', 'nextTrainsTowardsRome'],
+    ['towards-ostia', 'nextTrainsTowardsOstia']
 ];
 
 angular.module('starter.directives', [])
@@ -15,7 +15,19 @@ angular.module('starter.directives', [])
         templateUrl: '/templates/directive-station.html',
         replace: true,
         link: function(scope, el, attrs) {
-            Timetables.getLastNTimes(scope.stationId, DIRECTIONS[0], new XDate, 60);
+
+            var now = new XDate;
+
+            DIRECTIONS_TO_SCOPE.forEach(function(dir_and_scope) {
+                var direction = dir_and_scope[0]; var scopeName = dir_and_scope[1];
+                Timetables.getLastNTimes(scope.stationId, direction, now, 3)
+                    .then(function(tt) {
+                        var splits = tt.map(function(dt) {
+                            return "" + dt.getHours() + ":" + dt.getMinutes() +  " (in " + now.diffMinutes(dt) + " min)";
+                        });
+                        scope[scopeName] = splits.join(', ');
+                    });
+            });
         }
     };
 });
